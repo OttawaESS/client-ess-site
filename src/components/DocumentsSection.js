22305608de4aaf70899fc2646a41870928e3aa91
kandloic <jkand011@uottawa.ca>
2021-08-09 21:03:04 -0400

    cleaned up code + displayed doc titles

--- src/components/DocumentsSection.js
+++ src/components/DocumentsSection.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {classNames, toStyleObj, withPrefix} from '../utils';
-import SectionActions from './SectionActions';
+import {getData, classNames, toStyleObj, withPrefix} from '../utils';
 import Title from 'antd/lib/typography/Title';
 import { Collapse, Radio } from 'antd';
-import {getData} from '../utils';
 
 const { Panel } = Collapse;
 
@@ -18,7 +16,6 @@ export default class DocumentsSection extends React.Component {
     }
 
     handleOnChange = (e) => {
-      // console.log(e);
       this.setState({ activeMenu: e.target.value })
     }
 
@@ -30,15 +27,6 @@ export default class DocumentsSection extends React.Component {
         let background_opacity = background_opacity_pct * 0.01;
         let background_size = _.get(background, 'background_image_size', null) || 'cover';
         let background_repeat = _.get(background, 'background_image_repeat', null) || 'no-repeat';
-        let text = `
-        A dog is a type of domesticated animal.
-        Known for its loyalty and faithfulness,
-        it can be found as a welcome guest in many households across the world.
-        `
-        const docs = _.get(section, 'documents', null)
-        console.log(docs);
-
-        // let year = _.get(section, 'documents', null)
 
         return (
             <section className={classNames('section', 'hero', {'bg-image': _.get(section, 'has_background', null) && _.get(background, 'background_image', null), 'inverse bg-blue': _.get(section, 'has_background', null) && (background_color === 'blue'), 'bg-gray': _.get(section, 'has_background', null) && (background_color === 'gray'), 'section--padding': _.get(section, 'has_background', null) || _.get(section, 'image', null)})}>
@@ -50,26 +38,17 @@ export default class DocumentsSection extends React.Component {
                   {_.get(section, 'image', null) && (
                   <div className={classNames('cell', 'section__media', {'section__media--right': _.get(section, 'image_position', null) === 'right'})}>
                     {/* DOCUMENTS NAV HERE */}
-                    {/* <Collapse defaultActiveKey={['1']}>
-                      <Panel header="This is panel header 1" key="1">
-                        <p>{text}</p>
-                      </Panel>
-                      <Panel header="This is panel header 2" key="2">
-                        <p>{text}</p>
-                      </Panel>
-                      <Panel header="This is panel header 3" key="3">
-                        <p>{text}</p>
-                      </Panel>
-                    </Collapse> */}
                     {_.map(_.get(section, 'documents', null), (slide, slide_idx) => {
                       let slide_data = getData(this.props.pageContext.site.data, slide);
                       console.log(slide_data);
                       return (
-                        slide_data.year === this.state.activeMenu && <Collapse>
+                        slide_data.year === this.state.activeMenu && <Collapse key={slide_idx}>
                          {slide_data.types.map((type, idx) => (
-                           <Panel header={type.type}>
+                           <Panel header={type.type} key={idx}>
                              {type.files.map((file, id) => (
-                               <li>{file.label}</li>
+                               <li key={id}>
+                                 <a href={withPrefix(file.path)} target = "_blank" rel="noreferrer">{file.label}</a>
+                                 </li>
                              ))}
                            </Panel>
                          ))}
@@ -79,21 +58,20 @@ export default class DocumentsSection extends React.Component {
 
                   </div>
                   )}
+                  {/* NAVIGATION BUTTONS HERE */}
                   <div className="cell section__body">
                     {_.get(section, 'title', null) && (
                     <Title className="section__title">{_.get(section, 'title', null)}</Title>
                     )}
                     {_.get(section, 'actions', null) && (
-                    <div className="section__actions btn-group">
-                      {/* <SectionActions {...this.props} actions={_.get(section, 'actions', null)} /> */}
-                      <Radio.Group defaultValue={this.state.activeMenu} buttonStyle="solid" onChange={this.handleOnChange}>
+                    <div className="section__actions btn-group" style={{ display: 'block' }}>
+                      <Radio.Group defaultValue={this.state.activeMenu} buttonStyle="solid" onChange={this.handleOnChange} style={{ display: 'block' }}>
                         {_.get(section, 'actions', null).map((action, idx) => (
-                          <Radio.Button key={idx} value={action.label}>{action.label}</Radio.Button>
+                          <Radio.Button key={idx} value={action.label} style={{ display: 'block', margin: '0.5rem 0' }}>{action.label}</Radio.Button>
                         ))}
                       </Radio.Group>
                     </div>
                     )}
-                    {/* NAVIGATION BUTTONS HERE */}
                   </div>
                   <div>
                   
