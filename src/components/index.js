3383c27686b14eb6d4de8fd0e3e7d939f69cd64e
Cem Gurel <cgure085@uottawa.ca>
2021-08-17 23:17:29 +0300

    instructions template with dummy page

--- src/components/index.js
+++ src/components/index.js
@@ -1,60 +1,63 @@
-import Action from './Action';
-import BannerSection from './BannerSection';
-import BlogFeedItemFilter from './BlogFeedItemFilter';
-import BlogFeedSection from './BlogFeedSection';
-import BlogPostCategories from './BlogPostCategories';
-import BlogPostFeedItem from './BlogPostFeedItem';
-import BlogPostTags from './BlogPostTags';
-import ContactSection from './ContactSection';
-import ContentSection from './ContentSection';
-import CtaSection from './CtaSection';
-import FeaturesSection from './FeaturesSection';
-import Footer from './Footer';
-import Header from './Header';
-import HeroSection from './HeroSection';
-import SectionActions from './SectionActions';
-import PageMapSection from './PageMapSection';
-import TeamSection from './TeamSection';
-import Layout from './Layout';
+import Action from "./Action";
+import BannerSection from "./BannerSection";
+import BlogFeedItemFilter from "./BlogFeedItemFilter";
+import BlogFeedSection from "./BlogFeedSection";
+import BlogPostCategories from "./BlogPostCategories";
+import BlogPostFeedItem from "./BlogPostFeedItem";
+import BlogPostTags from "./BlogPostTags";
+import ContactSection from "./ContactSection";
+import ContentSection from "./ContentSection";
+import CtaSection from "./CtaSection";
+import FeaturesSection from "./FeaturesSection";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import SectionActions from "./SectionActions";
+import PageMapSection from "./PageMapSection";
+import TeamSection from "./TeamSection";
+import InstructionsSection from "./InstructionsSection";
+import Layout from "./Layout";
 
 export {
-    Action,
-    BannerSection,
-    BlogFeedItemFilter,
-    BlogFeedSection,
-    BlogPostCategories,
-    BlogPostFeedItem,
-    BlogPostTags,
-    ContactSection,
-    ContentSection,
-    CtaSection,
-    FeaturesSection,
-    Footer,
-    Header,
-    HeroSection,
-    SectionActions,
-    PageMapSection,
-    TeamSection,
-    Layout
+  Action,
+  BannerSection,
+  BlogFeedItemFilter,
+  BlogFeedSection,
+  BlogPostCategories,
+  BlogPostFeedItem,
+  BlogPostTags,
+  ContactSection,
+  ContentSection,
+  CtaSection,
+  FeaturesSection,
+  Footer,
+  Header,
+  HeroSection,
+  SectionActions,
+  PageMapSection,
+  TeamSection,
+  InstructionsSection,
+  Layout,
 };
 
 export default {
-    Action,
-    BannerSection,
-    BlogFeedItemFilter,
-    BlogFeedSection,
-    BlogPostCategories,
-    BlogPostFeedItem,
-    BlogPostTags,
-    ContactSection,
-    ContentSection,
-    CtaSection,
-    FeaturesSection,
-    Footer,
-    Header,
-    HeroSection,
-    PageMapSection,
-    SectionActions,
-    TeamSection,
-    Layout
+  Action,
+  BannerSection,
+  BlogFeedItemFilter,
+  BlogFeedSection,
+  BlogPostCategories,
+  BlogPostFeedItem,
+  BlogPostTags,
+  ContactSection,
+  ContentSection,
+  CtaSection,
+  FeaturesSection,
+  Footer,
+  Header,
+  HeroSection,
+  PageMapSection,
+  SectionActions,
+  TeamSection,
+  InstructionsSection,
+  Layout,
 };
