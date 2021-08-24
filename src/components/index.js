9cc632a7ec2d7d515989cf9256b6d5bca6eacda7
Cem Gurel <cgure085@uottawa.ca>
2021-08-24 11:40:03 -0400

    upcoming events section

--- src/components/index.js
+++ src/components/index.js
@@ -1,82 +1,84 @@
-import Action from './Action';
-import BannerSection from './BannerSection';
-import BlogFeedItemFilter from './BlogFeedItemFilter';
-import BlogFeedSection from './BlogFeedSection';
-import BlogPostCategories from './BlogPostCategories';
-import BlogPostFeedItem from './BlogPostFeedItem';
-import BlogPostTags from './BlogPostTags';
-import ContactSection from './ContactSection';
-import ContentSection from './ContentSection';
-import CtaSection from './CtaSection';
-import Document from './Document';
-import DocumentsSection from './DocumentsSection';
-import FeaturesSection from './FeaturesSection';
-import Footer from './Footer';
-import GridSection from './GridSection';
-import Header from './Header';
-import HeroSection from './HeroSection';
+import Action from "./Action";
+import BannerSection from "./BannerSection";
+import BlogFeedItemFilter from "./BlogFeedItemFilter";
+import BlogFeedSection from "./BlogFeedSection";
+import BlogPostCategories from "./BlogPostCategories";
+import BlogPostFeedItem from "./BlogPostFeedItem";
+import BlogPostTags from "./BlogPostTags";
+import ContactSection from "./ContactSection";
+import ContentSection from "./ContentSection";
+import CtaSection from "./CtaSection";
+import Document from "./Document";
+import DocumentsSection from "./DocumentsSection";
+import FeaturesSection from "./FeaturesSection";
+import Footer from "./Footer";
+import GridSection from "./GridSection";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
 import InstructionsSection from "./InstructionsSection";
-import LineDivider from './LineDivider';
-import SecondaryHeader from './SecondaryHeader';
-import SecondaryHeroSection from './SecondaryHeroSection';
-import SectionActions from './SectionActions';
-import PageMapSection from './PageMapSection';
-import TeamSection from './TeamSection';
-import Layout from './Layout';
-
+import LineDivider from "./LineDivider";
+import SecondaryHeader from "./SecondaryHeader";
+import SecondaryHeroSection from "./SecondaryHeroSection";
+import SectionActions from "./SectionActions";
+import UpcomingEvents from "./UpcomingEvents";
+import PageMapSection from "./PageMapSection";
+import TeamSection from "./TeamSection";
+import Layout from "./Layout";
 
 export {
-    Action,
-    BannerSection,
-    BlogFeedItemFilter,
-    BlogFeedSection,
-    BlogPostCategories,
-    BlogPostFeedItem,
-    BlogPostTags,
-    ContactSection,
-    ContentSection,
-    CtaSection,
-    Document,
-    DocumentsSection,
-    FeaturesSection,
-    Footer,
-    GridSection,
-    Header,
-    HeroSection,
-    InstructionsSection,
-    LineDivider,
-    SecondaryHeader,
-    SecondaryHeroSection,
-    SectionActions,
-    PageMapSection,
-    TeamSection,
-    Layout
+  Action,
+  BannerSection,
+  BlogFeedItemFilter,
+  BlogFeedSection,
+  BlogPostCategories,
+  BlogPostFeedItem,
+  BlogPostTags,
+  ContactSection,
+  ContentSection,
+  CtaSection,
+  Document,
+  DocumentsSection,
+  FeaturesSection,
+  Footer,
+  GridSection,
+  Header,
+  HeroSection,
+  InstructionsSection,
+  LineDivider,
+  SecondaryHeader,
+  SecondaryHeroSection,
+  SectionActions,
+  PageMapSection,
+  TeamSection,
+  UpcomingEvents,
+  Layout,
 };
 
 export default {
-    Action,
-    BannerSection,
-    BlogFeedItemFilter,
-    BlogFeedSection,
-    BlogPostCategories,
-    BlogPostFeedItem,
-    BlogPostTags,
-    ContactSection,
-    ContentSection,
-    CtaSection,
-    Document,
-    DocumentsSection,
-    FeaturesSection,
-    Footer,
-    GridSection,
-    Header,
-    HeroSection,
-    InstructionsSection,
-    LineDivider,
-    PageMapSection,
-    SecondaryHeader,
-    SecondaryHeroSection,
-    SectionActions,
-    TeamSection,
-    Layout
+  Action,
+  BannerSection,
+  BlogFeedItemFilter,
+  BlogFeedSection,
+  BlogPostCategories,
+  BlogPostFeedItem,
+  BlogPostTags,
+  ContactSection,
+  ContentSection,
+  CtaSection,
+  Document,
+  DocumentsSection,
+  FeaturesSection,
+  Footer,
+  GridSection,
+  Header,
+  HeroSection,
+  InstructionsSection,
+  LineDivider,
+  PageMapSection,
+  SecondaryHeader,
+  SecondaryHeroSection,
+  SectionActions,
+  TeamSection,
+  UpcomingEvents,
+  Layout,
 };
