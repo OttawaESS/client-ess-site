35c61028c4e63e42dcb52d000143e41ad4f10281
Harshit Manchanda <hmanchanda27@gmail.com>
2021-08-24 22:10:29 -0400

    moved title outside and changed positions per slide

--- src/components/PositionsSlider.js
+++ src/components/PositionsSlider.js
@@ -5,7 +5,7 @@ import styles from '../sass/components/positionsSlider.module.scss';
 import { Carousel } from 'antd';
 import {LeftOutlined, RightOutlined} from '@ant-design/icons';
 
-export default class SecondaryHeroSection extends React.Component {
+export default class PositionsSlider extends React.Component {
     constructor(props) {
         super(props);
         this.next = this.next.bind(this);
@@ -20,45 +20,36 @@ export default class SecondaryHeroSection extends React.Component {
 
     render() {
         let section = _.get(this.props, 'section', null);
-
         return (
             <div className="section" style={{ paddingTop: 0 }}>
                 <div className={styles.container}>
-                <LeftOutlined onClick={this.previous} className={styles.left_arrow} />
-                <Carousel autoplay className={styles.carousel} dots={false} ref={slide => (this.carousel = slide)} speed={1000} autoplaySpeed={5000}>
-                {_.map(_.get(section, 'positions', null), (position, position_idx) => {
-                    return (
-                    <div key={position_idx}>
+                    <div>
                         {_.get(section, 'title', null) && (
-                            <div className={styles.carousel_title}>{_.get(section, 'title', null)}</div>
+                            <h3 className={styles.carousel_title}>{_.get(section, 'title', null)}</h3>
                         )}
-                        <div className={styles.position_section}>
-                            <div className={styles.position_left}>
-                                {_.get(position, 'title_1', null) && (
-                                    <div className={styles.position_left_title}>{_.get(position, 'title_1', null)}</div>
+                    </div>
+                    <div className={styles.carousel_container}>
+                    <LeftOutlined onClick={this.previous} className={styles.left_arrow} />
+                    <Carousel autoplay className={styles.carousel} dots={false} ref={slide => (this.carousel = slide)} speed={1000} autoplaySpeed={5000}>
+                    {_.map(_.get(section, 'positions', null), (position, position_idx) => {
+                        return (
+                        <div key={position_idx}>
+                            <div className={styles.position_section}>
+                                {_.get(position, 'title', null) && (
+                                    <div className={styles.position_title}>{_.get(position, 'title', null)}</div>
                                 )}
-                                {_.get(position, 'content_1', null) && (
-                                    <div className={styles.position_left_info}>
-                                    {markdownify(_.get(position, 'content_1', null))}
-                                    </div>
-                                )}
-                            </div>
-                            <div className={styles.position_right}>
-                                {_.get(position, 'title_2', null) && (
-                                    <div className={styles.position_right_title}>{_.get(position, 'title_2', null)}</div>
-                                )}
-                                {_.get(position, 'content_2', null) && (
-                                    <div className={styles.position_right_info}>
-                                    {markdownify(_.get(position, 'content_2', null))}
+                                {_.get(position, 'content', null) && (
+                                    <div className={styles.position_info}>
+                                    {markdownify(_.get(position, 'content', null))}
                                     </div>
                                 )}
                             </div>
                         </div>
+                    )
+                    })}
+                    </Carousel>
+                    <RightOutlined className={styles.right_arrow} onClick={this.next}/>
                     </div>
-                   )
-                })}
-                </Carousel>
-                <RightOutlined className={styles.right_arrow} onClick={this.next}/>
                 </div>
             </div>
         );
--- src/pages/positions-slider.md
+++ src/pages/positions-slider.md
@@ -4,14 +4,14 @@ sections:
    - type: positions_slider
      title: Available Positions
      positions:
-      - title_1: Dusted DD
-        content_1: >-
+      - title: Dusted DD
+        content: >-
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim integer augue justo morbi ut arcu, diam, luctus ante. Velit tristique risus sit dignissim nam lacus, id molestie velit.
-        title_2: External VP
-        content_2: >-
+      - title: External VP
+        content: >-
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim integer augue justo morbi ut arcu, diam, luctus ante. Velit tristique risus sit dignissim nam lacus, id molestie velit.
-      - title_1: Ex CEO
-        content_1: >-
+      - title: Ex CEO
+        content: >-
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim integer augue justo morbi ut arcu, diam, luctus ante. Velit tristique risus sit dignissim nam lacus, id molestie velit.
 template: advanced
 ---
