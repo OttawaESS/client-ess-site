af1347b8b872f9c9116f35272a28a82f355d2479
Adhish Maheswaran <amahe023@uottawa.ca>
2021-08-25 09:17:52 -0400

    Team component

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
-
+import 'antd/dist/antd.css';
+import { Card} from 'antd';
 import {getData, withPrefix, markdownify} from '../utils';
-
+const { Meta } = Card;
 export default class TeamSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
@@ -13,32 +14,56 @@ export default class TeamSection extends React.Component {
                 <h2 className="section__title">{_.get(section, 'title', null)}</h2>
               </div>
               )}
-              <div className="container container--lg">
-                <div className="flex flex--col-2">
-                  {_.map(_.get(section, 'team', null), (person, person_idx) => {
-                      let person_data = getData(this.props.pageContext.site.data, person);
-                      return (
-                      <div key={person_idx} className="cell">
-                        <div className="card team-member">
-                          {person_data.photo && (
-                          <figure className="card__media card__media--bottom">
-                            <img src={withPrefix(person_data.photo)} alt={person_data.photo_alt} />
-                          </figure>
-                          )}
-                          <div className="card__body">
-                            <header className="card__header">
-                              <h3 className="h4 card__title">{person_data.first_name} {person_data.last_name}</h3>
-                            </header>
-                            {person_data.bio && (
-                            <div className="card__copy">
-                              {markdownify(person_data.bio)}
-                            </div>
-                            )}
-                          </div>
-                        </div>
-                      </div>
-                      )
-                  })}
+              <div className="container container--lg align-center">
+                <div className="flex flex--col-3">
+                        {/*{_.map(
+                            _.get(section, "team", null),
+                            (person, person_idx) => {
+                                let person_data = getData(
+                                    this.props.pageContext.site.data,
+                                    person
+                                );*/}
+                                {_.map(_.get(section, 'team', null), (person, person_idx)=> {
+                                let person_data = getData(this.props.pageContext.site.data, person);
+                                return (
+
+                                  <Card
+                                    style={{ width: 300,
+                                      justifyContent: 'center',
+                                      alignItems: 'center',
+                                      align: 'center'
+                                    }}
+                                    cover={
+                                      <img
+                                        alt="example"
+                                        src={withPrefix(person_data.photo)}
+                                        style={{
+                                          width: 200,
+                                          height: 200,
+                                          borderRadius:200/2,
+                                          justifyContent: 'center',
+                                          alignItems: 'center',
+                                          align: 'center'}}
+                                      />
+                                    }
+                                  >
+                                  <Meta
+                                    title={person_data.first_name}
+                                    description={markdownify(person_data.bio)}
+                                  />
+                                  </Card>
+                                  /*<div>
+                                        key={person_idx}
+                                        className="cell"
+                                        style={{
+                                            paddingRight: "0.75rem",
+                                            paddingLeft: "0.75rem",
+                                        }}
+                                      </div>*/
+                                );
+                              }
+                            
+                        )}
                 </div>
               </div>
             </section>
