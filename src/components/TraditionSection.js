105c28a8f947a41c067ea3cae84150cb16fc6ded
Cyrus Choi <77631273+cyruschoisy@users.noreply.github.com>
2024-08-05 20:20:30 -0400

    Update TraditionSection.js

--- src/components/TraditionSection.js
+++ src/components/TraditionSection.js
@@ -10,47 +10,47 @@ export default function TraditionSection(props) {
   let section = _.get(props, 'section', null);
 
   return (
-      <section className="section section--features">
+      <section className="section section--traditions">
         {_.get(section, 'title', null) && (
         <div className="container container--md align-center">
           <h2 className="section__title">{_.get(section, 'title', null)}</h2>
         </div>
         )}
         <div className="container container--lg">
-          {_.map(_.get(section, 'features', null), (feature, feature_idx) => (        
-          <FeatureWrapper key={feature_idx}>
-            <div className={classNames('flex', 'flex--middle', 'flex--center', 'flex--col-2', {'align-center': _.get(feature, 'align', null) === 'center', 'align-right': _.get(feature, 'align', null) === 'right'})}>
-              {_.get(feature, 'image', null) && (
-              <div className={classNames('cell', 'section__media', {'section__media--right': _.get(feature, 'image_position', null) === 'right'})}>
-                <img src={withPrefix(_.get(feature, 'image', null))} alt={_.get(feature, 'image_alt', null)} width={50}/>
+          {_.map(_.get(section, 'traditions', null), (tradition, tradition_idx) => (        
+          <TraditionWrapper key={tradition_idx}>
+            <div className={classNames('flex', 'flex--middle', 'flex--center', 'flex--col-2', {'align-center': _.get(tradition, 'align', null) === 'center', 'align-right': _.get(tradition, 'align', null) === 'right'})}>
+              {_.get(tradition, 'image', null) && (
+              <div className={classNames('cell', 'section__media', {'section__media--right': _.get(tradition, 'image_position', null) === 'right'})}>
+                <img src={withPrefix(_.get(tradition, 'image', null))} alt={_.get(tradition, 'image_alt', null)} width={50}/>
               </div>
               )}
               <div className="section__body cell">
-                {_.get(feature, 'title', null) && (
+                {_.get(tradition, 'title', null) && (
                   _.get(section, 'title', null) ? (
-                  <h4 className="section__title">{_.get(feature, 'title', null)}</h4>
+                  <h4 className="section__title">{_.get(tradition, 'title', null)}</h4>
                   ) : 
-                  <h2 className="section__title">{_.get(feature, 'title', null)}</h2>
+                  <h2 className="section__title">{_.get(tradition, 'title', null)}</h2>
                 )}
-                {_.get(feature, 'content', null) && (
+                {_.get(tradition, 'content', null) && (
                 <div className="section__copy">
-                  {markdownify(_.get(feature, 'content', null))}
+                  {markdownify(_.get(tradition, 'content', null))}
                 </div>
                 )}
-                {_.get(feature, 'actions', null) && (
+                {_.get(tradition, 'actions', null) && (
                 <div className="section__actions btn-group">
-                  <SectionActions {...props} actions={_.get(feature, 'actions', null)} />
+                  <SectionActions {...props} actions={_.get(tradition, 'actions', null)} />
                 </div>
                 )}
               </div>                  
             </div>
-          </FeatureWrapper>
+          </TraditionWrapper>
           ))}
         </div>
       </section>
   );
 }
 
-const FeatureWrapper = styled.div`
+const TraditionWrapper = styled.div`
   padding-bottom: 3.75rem;
 `;
